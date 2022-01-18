06e35a8b6607f341abf6c1d81f33352e7938d1f4
harshalp98 <harshalpawar330@gmail.com>
2022-01-18 22:45:37 +0530

    Day2

---
+++ RazorViewEngineDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewEngineDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.CustId = 111;
+            ViewBag.CustName = "Alex";
+            ViewBag.Amount = 25000;
+            ViewBag.Type = new List<string>() { "Saving", "Current","FD"};
+            return View();
+        }
+    }
+}
