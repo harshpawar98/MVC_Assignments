a380b57b572b6ce5b1cfb6149a0b784a67e152f7
harshalp98 <harshalpawar330@gmail.com>
2022-01-19 17:22:19 +0530

    Day3

---
+++ UrlRoutingDemo/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlRoutingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult empDetails(int id)
+        {
+            var emp =new [] { 
+            new { empid=1,empname="alex",deptid=101},
+            new { empid=2,empname="roli",deptid=106},
+            new { empid=5,empname="james",deptid=101},
+            };
+            string ename = "";
+            foreach (var e in emp)
+            {
+                if (e.empid == id)
+                {
+                    ename = e.empname;
+                }
+             }
+            return Content(ename);
+        }
+
+        //Id as string parameter
+        public ActionResult empData(string id)
+        {
+            var emp = new[] {
+            new { empid=1,empname="alex",deptid=101},
+            new { empid=2,empname="roli",deptid=106},
+            new { empid=5,empname="james",deptid=101},
+            };
+            int eid = 0;
+            foreach (var e in emp)
+            {
+                if (e.empname == id)
+                {
+                    eid = e.empid;
+                }
+            }
+            return Content(eid.ToString());
+        }
+
+
+    }
+}
---
+++ UrlRoutingDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UrlRoutingDemo.MvcApplication" Language="C#" %>
