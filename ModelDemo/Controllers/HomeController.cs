7206b30d7e96593255eb6b8b16f32bdf4d6ba467
harshalp98 <harshalpawar330@gmail.com>
2022-01-20 19:27:59 +0530

    Day4

---
+++ ModelDemo/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelDemo.Models;
+
+namespace ModelDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Product> product = new List<Product>() {
+
+                new Product(){productid=101,productname="Earphone",price=999 },
+                new Product(){productid=102,productname="Laptop",price=99999 },
+                new Product(){productid=103,productname="Mouse",price=499 }
+            };
+            //ViewBag.Products = product;
+
+            return View(product);
+        }
+
+        public ActionResult Details(int id)
+        {
+            List<Product> product = new List<Product>() {
+
+                new Product(){productid=101,productname="Earphone",price=999 },
+                new Product(){productid=102,productname="Laptop",price=99999 },
+                new Product(){productid=103,productname="Mouse",price=499 }
+            };
+
+            Product matchproduct=null;
+            foreach (var pr in product)
+            {
+                if (pr.productid == id)
+                {
+                    matchproduct = pr;
+                }
+            }
+            //ViewBag.MatchProduct = matchproduct;
+            return View(matchproduct);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //create product
+        [HttpPost]
+        public ActionResult Create(int productid,string productname,int price)
+        {
+            return View();
+        }
+    }
+}
---
+++ ModelDemo/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelDemo.Models
+{
+    public class Product
+    {
+        public int productid { get; set; }
+        public string productname { get; set; }
+        public int price { get; set; }
+    }
+}
