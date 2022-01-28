d39e36ea4dd979b7288ab15b7d3a2686256ed6cf
harshalp98 <harshalpawar330@gmail.com>
2022-01-28 18:20:31 +0530

    Day8

---
+++ TempDataDemo1/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataDemo1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            TempData["Message"] = "Hello Welcome";
+            return RedirectToAction("Index2");
+        }
+        public ActionResult Index2()
+        {
+            string s = Convert.ToString(TempData["Message"]);
+            ViewBag.mess = s;
+            return View();
+        }
+    }
+}
---
+++ TempDataDemo1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            TempData["Message"] = "Hello";
+            return RedirectToAction("Index2");
+        }
+        public ActionResult Index2()
+        {
+            //TempData.Keep("Message");
+            TempData.Peek("Message");
+            return RedirectToAction("Index3");
+        }
+        public ActionResult Index3()
+        {
+            string s = Convert.ToString(TempData["Message"]);
+            ViewBag.mess = s;
+            return View();
+        }
+    }
+}
